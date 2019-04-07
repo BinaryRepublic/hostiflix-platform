dc20ab21b8a1b3b6895ebb0548a15244236f67f2
Tobias Trinks <tobias.trinks99@gmx.net>
2019-04-07 14:30:09 +0200

    Reduce node count to 2, add postgres database and user

--- gcp/postgres.tf
+++ gcp/postgres.tf
@@ -1,9 +1,20 @@
 resource "google_sql_database_instance" "master" {
-  name = "hostiflix"
+  name = "production"
   database_version = "POSTGRES_9_6"
   region = "europe-west3"
 
   settings {
     tier = "db-f1-micro"
   }
+}
+
+resource "google_sql_database" "users" {
+  name      = "hostiflix"
+  instance  = "${google_sql_database_instance.master.name}"
+}
+
+resource "google_sql_user" "users" {
+  instance = "${google_sql_database_instance.master.name}"
+  name     = "postgres"
+  password = "password"
 }
